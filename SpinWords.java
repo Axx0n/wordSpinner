a9a319defc693300199ac47748dc8501fca5cbe2
William <willstep13@gmail.com>
2019-04-14 14:52:27 -0500

    Create SpinWords.java

---
+++ SpinWords.java
@@ -0,0 +1,16 @@
+//A better solution to improve towards
+
+import java.util.Arrays;
+
+public class SpinWords {
+
+  public String spinWords(String sentence) {
+    String[] words = sentence.split(" ");
+    for (int i=0; i<words.length; i++) {
+      if (words[i].length() >= 5) {
+        words[i] = new StringBuilder(words[i]).reverse().toString();
+      }
+    }
+    return String.join(" ",words);
+  }
+}
